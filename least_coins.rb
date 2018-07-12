f4791b2ae874220f32edafce46d045849969a7f3
keelyvent <kvent11@yahoo.com>
2018-07-12 16:37:14 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,6 +2,14 @@
 
 def least_coins(cents)
 
-#Code your answer here!
-
+  coins = {}
+  coins[:quarters] = cents/25 
+  cents = cents%25
+  coins[:dimes] = cents/10
+  cents = cents%10 
+  coins[:nickel]=cents/5 
+  coins[:pennies] =cents%5 
+  puts coins
 end
+
+puts least_coins(29)
